59b7b1e6fc7651a3a9d68c4352cd5d375884c520
Yuri Tkachenko <yuri@ya.ru>
2015-10-23 14:02:20 +0300

    structure

---
+++ js/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+// import LoginHandler from './components/Login.js';
+
+let App = React.createClass({
+  render() {
+    return (
+      <div className="app">
+
+      </div>
+    );
+  }
+});
+
+React.render(<App/>, document.querySelector('.app'));
